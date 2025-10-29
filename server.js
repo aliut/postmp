df760d358ca4fc75df330fa26bd01f54a9b695a0
aliut <73517589+aliut@users.noreply.github.com>
2025-10-29 18:25:12 +0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 const multer = require('multer');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // File upload configuration
 const upload = multer({ dest: 'uploads/' });
